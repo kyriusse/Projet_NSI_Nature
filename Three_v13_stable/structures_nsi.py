4d8c5c471cf82b19c1a67aa3de4aaef9750b5ee3
eguzkiba <eguzkiba@gmail.com>
2026-03-22 10:51:47 +0100

    structures_nsi.py

--- Three_v13_stable/structures_nsi.py
+++ Three_v13_stable/structures_nsi.py
@@ -1,35 +1,35 @@
 from __future__ import annotations
 
-from collections import deque
+from collections import deque #importe deque une structure de données pour les files 
 
 
-class Pile:
+class Pile: #définition de la classe Pile 
     def __init__(self):
-        self._donnees = []
+        self._donnees = []  #créer une liste vide pour stocker les éléments de la pile
 
     def empiler(self, valeur):
-        self._donnees.append(valeur)
+        self._donnees.append(valeur) #ajoute une valeur au somment de la pile 
 
     def depiler(self):
-        if self.est_vide():
-            return None
-        return self._donnees.pop()
+        if self.est_vide():  #vérifie si la pile est vide 
+            return None #si elle est vide on retourne None 
+        return self._donnees.pop() #enlève et retourne le dernier élément ajouté 
 
     def est_vide(self):
-        return len(self._donnees) == 0
+        return len(self._donnees) == 0  #retourne True si la pile est vide sinon False 
 
 
-class File:
+class File: #définition de la classe File 
     def __init__(self):
-        self._donnees = deque()
+        self._donnees = deque()  #Initialise une deque pour stocker les éléments de la file 
 
     def enfiler(self, valeur):
-        self._donnees.append(valeur)
+        self._donnees.append(valeur) #ajoute une valeur à la fin de la file 
 
     def defiler(self):
-        if self.est_vide():
-            return None
-        return self._donnees.popleft()
+        if self.est_vide():   #vérifie si la file est vide 
+            return None  #si elle l'est on retourne None 
+        return self._donnees.popleft()  #on retire et retourne le le premier élément ajouté 
 
     def est_vide(self):
-        return len(self._donnees) == 0
+        return len(self._donnees) == 0  #Retourne True si la file est vide sinon cela retourne False 
